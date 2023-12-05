5ba7e171dc3369fbe245ea433a2cd6f9753624a6
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-05 20:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function isSameType(value1, value2) {
   //your js code here
-	 const bothAreNaN = isNaN(value1) && isNaN(value2);
-
-  if (bothAreNaN) {
+	// Check if both values are NaN
+  if (isNaN(value1) && isNaN(value2)) {
     return true;
   }
 
-  const typeOfValue1 = typeof value1;
-  const typeOfValue2 = typeof value2;
-
-  return typeOfValue1 === typeOfValue2;
+  // Check if the types of both values are the same
+  return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
